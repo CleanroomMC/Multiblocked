9f093e3c5ff9705f441ea634b8452b5721b159c4
Rongmario <rongmario@protonmail.com>
2022-01-10 14:35:51 +0000

    Rebase + setup .minecraft/config/multiblocked/assets as resource loader

--- src/main/java/io/github/cleanroommc/multiblocked/client/ClientProxy.java
+++ src/main/java/io/github/cleanroommc/multiblocked/client/ClientProxy.java
@@ -3,16 +3,35 @@ package io.github.cleanroommc.multiblocked.client;
 import io.github.cleanroommc.multiblocked.CommonProxy;
 import io.github.cleanroommc.multiblocked.api.registry.MultiblockComponents;
 import io.github.cleanroommc.multiblocked.client.model.BakedModelHandler;
+import net.minecraft.client.resources.IResourcePack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 @SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit() {
+        super.preInit();
+        try {
+            Field resourcePackListField = FMLClientHandler.class.getDeclaredField("resourcePackList");
+            resourcePackListField.setAccessible(true);
+            List<IResourcePack> resourcePackList = (List<IResourcePack>) resourcePackListField.get(FMLClientHandler.instance());
+            resourcePackList.add(MultiblockedResourceLoader.INSTANCE);
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+    }
+
     @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
         MultiblockComponents.registerModels();
---
+++ src/main/java/io/github/cleanroommc/multiblocked/client/MultiblockedResourceLoader.java
@@ -0,0 +1,42 @@
+package io.github.cleanroommc.multiblocked.client;
+
+import com.google.gson.JsonObject;
+import net.minecraft.client.resources.FolderResourcePack;
+import net.minecraft.client.resources.data.IMetadataSection;
+import net.minecraft.client.resources.data.MetadataSerializer;
+import net.minecraftforge.fml.common.Loader;
+
+import java.io.File;
+
+public class MultiblockedResourceLoader extends FolderResourcePack {
+
+    private static final File location;
+
+    static {
+        location = new File(Loader.instance().getConfigDir(), "multiblocked");
+        location.mkdir();
+        new File(location, "assets").mkdir();
+    }
+
+    public static final MultiblockedResourceLoader INSTANCE = new MultiblockedResourceLoader();
+
+    private MultiblockedResourceLoader() {
+        super(location);
+    }
+
+    @Override
+    public String getPackName() {
+        return "MultiblockedInternalResources";
+    }
+
+    @Override
+    public <T extends IMetadataSection> T getPackMetadata(MetadataSerializer metadataSerializer, String metadataSectionName) {
+        JsonObject metadata = new JsonObject();
+        JsonObject packObj = new JsonObject();
+        metadata.add("pack", packObj);
+        packObj.addProperty("description", "Includes assets provided by the user for Multiblocked.");
+        packObj.addProperty("pack_format", 2);
+        return metadataSerializer.parseMetadataSection(metadataSectionName, metadata);
+    }
+
+}

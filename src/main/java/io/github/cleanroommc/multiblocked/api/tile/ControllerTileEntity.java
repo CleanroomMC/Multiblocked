5ad885d0a76321a70db1111803c5a970a99910fd
Yefancy <yefancy@foxmail.com>
2022-01-16 23:13:07 +0800

    Update ControllerTileEntity.java

--- src/main/java/io/github/cleanroommc/multiblocked/api/tile/ControllerTileEntity.java
+++ src/main/java/io/github/cleanroommc/multiblocked/api/tile/ControllerTileEntity.java
@@ -3,6 +3,8 @@ package io.github.cleanroommc.multiblocked.api.tile;
 import com.google.common.collect.Table;
 import com.google.common.collect.Tables;
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.mc1120.player.MCPlayer;
+import crafttweaker.mc1120.world.MCFacing;
 import io.github.cleanroommc.multiblocked.api.capability.CapabilityProxy;
 import io.github.cleanroommc.multiblocked.api.capability.IO;
 import io.github.cleanroommc.multiblocked.api.capability.MultiblockCapability;
@@ -10,6 +12,7 @@ import io.github.cleanroommc.multiblocked.api.definition.ControllerDefinition;
 import io.github.cleanroommc.multiblocked.api.pattern.MultiblockState;
 import io.github.cleanroommc.multiblocked.api.recipe.RecipeLogic;
 import io.github.cleanroommc.multiblocked.api.tile.part.PartTileEntity;
+import io.github.cleanroommc.multiblocked.gui.factory.TileEntityUIFactory;
 import io.github.cleanroommc.multiblocked.gui.modular.ModularUI;
 import io.github.cleanroommc.multiblocked.gui.texture.IGuiTexture;
 import io.github.cleanroommc.multiblocked.gui.util.ModularUIBuilder;
@@ -17,6 +20,7 @@ import io.github.cleanroommc.multiblocked.persistence.MultiblockWorldSavedData;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
@@ -196,22 +200,30 @@ public class ControllerTileEntity extends ComponentTileEntity<ControllerDefiniti
 
     @Override
     public boolean onRightClick(EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (super.onRightClick(player, hand, facing, hitX, hitY, hitZ)) return true;
+        if (definition.onRightClick != null) {
+            if (definition.onRightClick.apply(this, new MCPlayer(player), new MCFacing(facing), hitX, hitY, hitZ)) return true;
+        }
         if (!world.isRemote) {
-            if (isFormed()) return false;
-            if (state == null) state = new MultiblockState(world, pos);
-            ItemStack held = player.getHeldItem(hand);
-            if (definition.catalyst == null || held.isItemEqual(definition.catalyst)) {
-                if (checkPattern()) { // formed
-                    player.swingArm(hand);
-                    ITextComponent formedMsg = new TextComponentTranslation("multiblocked.multiblock.formed", getLocalizedName());
-                    player.sendStatusMessage(formedMsg, true);
-                    if (!player.isCreative() && definition.consumeCatalyst) {
-                        held.shrink(1);
+            if (!isFormed()) {
+                if (state == null) state = new MultiblockState(world, pos);
+                ItemStack held = player.getHeldItem(hand);
+                if (definition.catalyst == null || held.isItemEqual(definition.catalyst)) {
+                    if (checkPattern()) { // formed
+                        player.swingArm(hand);
+                        ITextComponent formedMsg = new TextComponentTranslation("multiblocked.multiblock.formed", getLocalizedName());
+                        player.sendStatusMessage(formedMsg, true);
+                        if (!player.isCreative() && definition.consumeCatalyst) {
+                            held.shrink(1);
+                        }
+                        MultiblockWorldSavedData.getOrCreate(world).addMapping(state);
+                        onStructureFormed();
+                        return true;
                     }
-                    MultiblockWorldSavedData.getOrCreate(world).addMapping(state);
-                    onStructureFormed();
-                    return true;
+                }
+            }
+            if (!player.isSneaking()) {
+                if (!world.isRemote && player instanceof EntityPlayerMP) {
+                    return TileEntityUIFactory.INSTANCE.openUI(this, (EntityPlayerMP) player);
                 }
             }
         }
@@ -220,7 +232,7 @@ public class ControllerTileEntity extends ComponentTileEntity<ControllerDefiniti
 
     @Override
     public ModularUI createUI(EntityPlayer entityPlayer) {
-        return new ModularUIBuilder(IGuiTexture.EMPTY, 500, 500)
+        return new ModularUIBuilder(IGuiTexture.EMPTY, 200, 150)
                 .label(10, 10, "Hello World", 0xffff00ff)
                 .bindPlayerInventory(entityPlayer.inventory, IGuiTexture.EMPTY, 10, 30)
                 .build(this, entityPlayer);

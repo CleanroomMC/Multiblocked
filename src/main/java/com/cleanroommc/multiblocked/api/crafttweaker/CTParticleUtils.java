5613370f69b91eb3f6afa00e946e01008904a956
Yefancy <yefancy@foxmail.com>
2022-04-13 16:09:49 +0800

    fix ct report on the server side

--- src/main/java/com/cleanroommc/multiblocked/api/crafttweaker/CTParticleUtils.java
+++ src/main/java/com/cleanroommc/multiblocked/api/crafttweaker/CTParticleUtils.java
@@ -24,34 +24,34 @@ import stanhebben.zenscript.annotations.ZenMethod;
 public class CTParticleUtils {
 
     @ZenMethod
-    @SideOnly(Side.CLIENT)
     public static ICTTextureParticle texture(IWorld world, double x, double y, double z, @Optional boolean isShader) {
         return isShader ? new ShaderTextureParticle(CraftTweakerMC.getWorld(world), x, y, z) : new CommonParticle(CraftTweakerMC.getWorld(world), x, y, z);
     }
 
     @ZenMethod
-    @SideOnly(Side.CLIENT)
     public static ICTTextureParticle texture(IWorld world, Position3f position, @Optional boolean isShader) {
         return isShader ? new ShaderTextureParticle(CraftTweakerMC.getWorld(world), position.getX(), position.getY(), position.getZ()) : new CommonParticle(CraftTweakerMC.getWorld(world), position.getX(), position.getY(), position.getZ());
     }
 
     @ZenMethod
-    @SideOnly(Side.CLIENT)
     public static ICTLaserParticle laser(IWorld world, double sX, double sY, double sZ, double eX, double eY, double eZ) {
         return new LaserBeamParticle(CraftTweakerMC.getWorld(world), new Vector3(sX, sY, sZ), new Vector3(eX, eY, eZ));
     }
 
     @ZenMethod
-    @SideOnly(Side.CLIENT)
     public static ICTLaserParticle laser(IWorld world, Position3f startPos, Position3f endPos) {
         return new LaserBeamParticle(CraftTweakerMC.getWorld(world), new Vector3(startPos.getX(), startPos.getY(), startPos.getZ()), new Vector3(endPos.getX(), endPos.getY(), endPos.getZ()));
     }
 
     @ZenMethod
-    @SideOnly(Side.CLIENT)
     public static IWorld getWorld() {
         World world = IParticleWidget.getWorld();
-        return CraftTweakerMC.getIWorld(world == null ? Minecraft.getMinecraft().world : world);
+        return CraftTweakerMC.getIWorld(world == null ? getMinecraftWorld() : world);
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static World getMinecraftWorld() {
+        return Minecraft.getMinecraft().world;
     }
 
 }

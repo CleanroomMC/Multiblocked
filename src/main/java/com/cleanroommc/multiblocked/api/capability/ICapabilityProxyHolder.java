178ea0b34f1c46bd41b4901a8bf84cf4c8bb5ef1
Yefancy <yefancy@foxmail.com>
2022-04-20 11:12:51 +0800

    exposed to ct

--- src/main/java/com/cleanroommc/multiblocked/api/capability/ICapabilityProxyHolder.java
+++ src/main/java/com/cleanroommc/multiblocked/api/capability/ICapabilityProxyHolder.java
@@ -1,11 +1,16 @@
 package com.cleanroommc.multiblocked.api.capability;
 
+import com.cleanroommc.multiblocked.Multiblocked;
 import com.cleanroommc.multiblocked.api.capability.proxy.CapabilityProxy;
 import com.google.common.collect.Table;
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.api.world.IBlockPos;
+import crafttweaker.api.world.IWorld;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenGetter;
 import stanhebben.zenscript.annotations.ZenMethod;
@@ -26,8 +31,13 @@ public interface ICapabilityProxyHolder {
 
     Table<IO, MultiblockCapability<?>, Long2ObjectOpenHashMap<CapabilityProxy<?>>> getCapabilities();
 
+    static ICapabilityProxyHolder fromWorldPos(World world, BlockPos pos, MultiblockCapability<?>... capabilities) {
+        return new CommonCapabilityProxyHolder(world, pos, capabilities);
+    }
+
     @ZenMethod
-    static ICapabilityProxyHolder fromWorldPos(World world, BlockPos pos, MultiblockCapability<?>... capability) {
-        return new CommonCapabilityProxyHolder(world, pos, capability);
+    @Optional.Method(modid = Multiblocked.MODID_CT)
+    static ICapabilityProxyHolder fromWorldPos(IWorld world, IBlockPos pos, MultiblockCapability<?>... capabilities) {
+        return fromWorldPos(CraftTweakerMC.getWorld(world), CraftTweakerMC.getBlockPos(pos), capabilities);
     }
 }

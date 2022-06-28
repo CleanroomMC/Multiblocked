e19d36f55c5f831a7d19f50569a07e482bd81e54
youyihj <youyi580@qq.com>
2022-06-28 13:36:58 +0800

    document

--- src/main/java/com/cleanroommc/multiblocked/api/capability/trait/InterfaceUser.java
+++ src/main/java/com/cleanroommc/multiblocked/api/capability/trait/InterfaceUser.java
@@ -6,10 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Mark a {@link CapabilityTrait} that the target mod requires tile entities to implement its interface to use its system.
+ *
  * @author youyihj
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface InterfaceUser {
+    /**
+     * The interface that the target mod requires to implement. The trait should implement the interface.
+     */
     Class<?> value();
 }

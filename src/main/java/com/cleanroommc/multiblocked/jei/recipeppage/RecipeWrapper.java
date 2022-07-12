17902839cd2af3350db3865f9b953f28930ee111
youyihj <youyi580@qq.com>
2022-07-12 11:03:02 +0800

    Close #46

--- src/main/java/com/cleanroommc/multiblocked/jei/recipeppage/RecipeWrapper.java
+++ src/main/java/com/cleanroommc/multiblocked/jei/recipeppage/RecipeWrapper.java
@@ -1,11 +1,18 @@
 package com.cleanroommc.multiblocked.jei.recipeppage;
 
+import com.cleanroommc.multiblocked.api.gui.ingredient.IIngredientSlot;
+import com.cleanroommc.multiblocked.api.gui.widget.Widget;
+import com.cleanroommc.multiblocked.api.gui.widget.imp.DraggableScrollableWidgetGroup;
 import com.cleanroommc.multiblocked.api.gui.widget.imp.recipe.RecipeWidget;
 import com.cleanroommc.multiblocked.api.recipe.Content;
 import com.cleanroommc.multiblocked.api.recipe.Recipe;
 import com.cleanroommc.multiblocked.jei.IJeiIngredientAdapter;
+import com.cleanroommc.multiblocked.jei.JeiPlugin;
 import com.cleanroommc.multiblocked.jei.ModularWrapper;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IFocus;
+import mezz.jei.input.MouseHelper;
+import net.minecraft.client.Minecraft;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -14,10 +21,12 @@ import java.util.stream.Collectors;
 public class RecipeWrapper extends ModularWrapper {
 
     public final Recipe recipe;
+    public final RecipeWidget widget;
 
     public RecipeWrapper(RecipeWidget widget) {
         super(widget, widget.getSize().width, widget.getSize().height);
-        recipe = widget.recipe;
+        this.widget = widget;
+        this.recipe = widget.recipe;
     }
 
     @Override
@@ -45,5 +54,37 @@ public class RecipeWrapper extends ModularWrapper {
                 ingredients.setOutput(adapter.getJeiIngredientType(), jeiIngredients);
             }
         });
+    }
+
+    @Override
+    public boolean handleClick(Minecraft minecraft, int mouseX, int mouseY, int mouseButton) {
+        // needs gui screen xy position
+        mouseX = MouseHelper.getX();
+        mouseY = MouseHelper.getY();
+        IFocus.Mode mode;
+        if (mouseButton == 0) {
+            mode = IFocus.Mode.OUTPUT;
+        } else if (mouseButton == 1) {
+            mode = IFocus.Mode.INPUT;
+        } else return false;
+        IIngredientSlot clicked = findClickedSlot(widget.inputs, mouseX, mouseY);
+        if (clicked == null) clicked = findClickedSlot(widget.outputs, mouseX, mouseY);
+        if (clicked != null) {
+            Object ingredient = clicked.getIngredientOverMouse(mouseX, mouseY);
+            if (ingredient != null) {
+                JeiPlugin.jeiRuntime.getRecipesGui().show(JeiPlugin.jeiRuntime.getRecipeRegistry().createFocus(mode, ingredient));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private IIngredientSlot findClickedSlot(DraggableScrollableWidgetGroup group, int mouseX, int mouseY) {
+        for (Widget w : group.widgets) {
+            if (w instanceof IIngredientSlot && w.isMouseOverElement(mouseX, mouseY)) {
+                return ((IIngredientSlot) w);
+            }
+        }
+        return null;
     }
 }

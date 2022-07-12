3dc78ea667500c2c2c05fe649dd3c9c365c17b8c
youyihj <youyi580@qq.com>
2022-07-12 23:16:31 +0800

    optional method

--- src/main/java/com/cleanroommc/multiblocked/jei/IJeiIngredientAdapter.java
+++ src/main/java/com/cleanroommc/multiblocked/jei/IJeiIngredientAdapter.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.multiblocked.jei;
 
 import mezz.jei.api.recipe.IIngredientType;
+import net.minecraftforge.fml.common.Optional;
 
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -11,5 +12,6 @@ import java.util.stream.Stream;
 public interface IJeiIngredientAdapter<T, R> extends Function<T, Stream<R>> {
     Class<T> getInternalIngredientType();
 
+    @Optional.Method(modid = "jei")
     IIngredientType<R> getJeiIngredientType();
 }

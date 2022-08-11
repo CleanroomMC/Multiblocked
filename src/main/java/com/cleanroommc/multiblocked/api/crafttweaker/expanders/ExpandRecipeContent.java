9055b2b893065f93c0c050e22de0b7550def6e4d
youyihj <youyi580@qq.com>
2022-08-11 19:34:34 +0800

    fix asIIngredient method uses NCO code

--- src/main/java/com/cleanroommc/multiblocked/api/crafttweaker/expanders/ExpandRecipeContent.java
+++ src/main/java/com/cleanroommc/multiblocked/api/crafttweaker/expanders/ExpandRecipeContent.java
@@ -16,17 +16,18 @@ import lach_01298.qmd.particle.ParticleStack;
 import mekanism.api.gas.GasStack;
 import mekanism.common.integration.crafttweaker.gas.CraftTweakerGasStack;
 import mekanism.common.integration.crafttweaker.gas.IGasStack;
-import nc.recipe.ingredient.OreIngredient;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.Optional;
 import stanhebben.zenscript.annotations.ZenCaster;
 import stanhebben.zenscript.annotations.ZenExpansion;
+import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenRegister
 @ZenExpansion("mods.multiblocked.recipe.Content")
 public class ExpandRecipeContent {
 
     @ZenCaster
+    @ZenMethod
     public static String asString(Content content) {
         if (content.content instanceof String) {
             return (String) content.content;
@@ -35,6 +36,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static Integer asInteger(Content content) {
         if (content.content instanceof Integer) {
             return (Integer) content.content;
@@ -43,6 +45,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static Float asFloat(Content content) {
         if (content.content instanceof Float) {
             return (Float) content.content;
@@ -51,6 +54,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static Double asDouble(Content content) {
         if (content.content instanceof Double) {
             return (Double) content.content;
@@ -59,6 +63,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static Boolean asBoolean(Content content) {
         if (content.content instanceof Boolean) {
             return (Boolean) content.content;
@@ -67,18 +72,21 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static IIngredient asIIngredient(Content content) {
         if (content.content instanceof ItemsIngredient) {
+            ItemsIngredient ingredient = (ItemsIngredient) content.content;
             if (((ItemsIngredient) content.content).isOre()) {
-                return CraftTweakerMC.getIIngredient(new OreIngredient(((ItemsIngredient) content.content).getOreDict(), ((ItemsIngredient) content.content).getAmount()));
+                return CraftTweakerMC.getOreDict(ingredient.getOreDict()).amount(ingredient.getAmount());
             } else {
-                return CraftTweakerMC.getIIngredient(content.content);
+                return CraftTweakerMC.getIIngredient(content.content).amount(ingredient.getAmount());
             }
         }
         return null;
     }
 
     @ZenCaster
+    @ZenMethod
     public static IItemStack asIItemStack(Content content) {
         if (content.content instanceof ItemsIngredient) {
             return CraftTweakerMC.getIItemStack(((ItemsIngredient) content.content).getOutputStack());
@@ -87,6 +95,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     public static ILiquidStack asILiquidStack(Content content) {
         if (content.content instanceof FluidStack) {
             return CraftTweakerMC.getILiquidStack((FluidStack) content.content);
@@ -95,6 +104,7 @@ public class ExpandRecipeContent {
     }
 
     @ZenCaster
+    @ZenMethod
     @Optional.Method(modid = Multiblocked.MODID_TC6)
     public static AspectStack asAspectStack(Content content) {
         if (content.content instanceof AspectStack) {
@@ -109,6 +119,7 @@ public class ExpandRecipeContent {
     public static class Gas {
         @ZenCaster
         @Optional.Method(modid = Multiblocked.MODID_MEK)
+        @ZenMethod
         public static IGasStack asIGasStack(Content content) {
             if (content.content instanceof GasStack) {
                 return new CraftTweakerGasStack((GasStack) content.content);
@@ -123,6 +134,7 @@ public class ExpandRecipeContent {
     public static class Particle {
 
         @ZenCaster
+        @ZenMethod
         @Optional.Method(modid = Multiblocked.MODID_QMD)
         public static IParticleStack asIParticleStack(Content content) {
             if (content.content instanceof ParticleStack) {

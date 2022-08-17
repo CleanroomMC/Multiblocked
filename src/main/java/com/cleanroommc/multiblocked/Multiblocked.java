6fe5d97db6a93c6683d8aeecd34bda90d90e9df8
youyihj <youyi580@qq.com>
2022-08-17 11:55:49 +0800

    version 0.6.6

--- src/main/java/com/cleanroommc/multiblocked/Multiblocked.java
+++ src/main/java/com/cleanroommc/multiblocked/Multiblocked.java
@@ -57,7 +57,7 @@ public class Multiblocked {
     public static final String MODID_PNC = "pneumaticcraft";
     public static final String MODID_AS = "astralsorcery";
     public static final String NAME = "Multiblocked";
-    public static final String VERSION = "0.6.5b";
+    public static final String VERSION = "0.6.6";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final Random RNG = new Random();
     public static final Gson GSON_PRETTY = new GsonBuilder().setPrettyPrinting().create();

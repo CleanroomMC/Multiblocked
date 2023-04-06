d5e305673a263bebfb1f603214e707e4e4093fd1
Rongmario <rongmario@protonmail.com>
2023-04-06 01:30:42 +0100

    Bump to 0.8.2

--- src/main/java/com/cleanroommc/multiblocked/Multiblocked.java
+++ src/main/java/com/cleanroommc/multiblocked/Multiblocked.java
@@ -57,7 +57,7 @@ public class Multiblocked {
     public static final String MODID_PNC = "pneumaticcraft";
     public static final String MODID_AS = "astralsorcery";
     public static final String NAME = "Multiblocked";
-    public static final String VERSION = "0.8.1";
+    public static final String VERSION = "0.8.2";
     public static final Logger LOGGER = LogManager.getLogger(NAME);
     public static final Random RNG = new Random();
     public static final Gson GSON_PRETTY = new GsonBuilder().setPrettyPrinting().create();

ec84225998411d8e561cf800aa4b4ec0dcac46f4
vfyjxf <42764413+vfyjxf@users.noreply.github.com>
2023-08-21 12:05:14 +0800

    ordering recipes (#90)
    * ordering recipes
    
    * improve code

--- src/main/java/com/cleanroommc/multiblocked/api/recipe/RecipeMap.java
+++ src/main/java/com/cleanroommc/multiblocked/api/recipe/RecipeMap.java
@@ -10,6 +10,7 @@ import com.cleanroommc.multiblocked.util.FileUtility;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import crafttweaker.annotations.ZenRegister;
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenConstructor;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -54,7 +56,7 @@ public class RecipeMap {
         register(EMPTY);
     }
 
-    public HashMap<String, Recipe> recipes = new HashMap<>();
+    public Map<String, Recipe> recipes = new HashMap<>();
 
     @ZenConstructor
     public RecipeMap(String name) {
@@ -164,6 +166,14 @@ public class RecipeMap {
     @ZenMethod
     public List<Recipe> allRecipes() {
         return new ArrayList<>(recipes.values());
+    }
+
+    public void sort(){
+        this.recipes = recipes.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, Object2ObjectLinkedOpenHashMap::new));
     }
 
 }
